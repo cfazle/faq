509eed94c9a78c273e7c25a67b19d8ff735cd5a4
Fazle Choudhury <fac3@njit.edu>
2018-07-17 12:43:03 -0400

    The question's view button linked to answer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -90,7 +90,11 @@
     </nav>
 
     <main class="py-4">
-
+        <div class="col-12">
+         @include('flash.messages')
+        @include('flash.error')
+        @include('flash.status')
+        </div>
         @yield('content')
     </main>
 </div>
